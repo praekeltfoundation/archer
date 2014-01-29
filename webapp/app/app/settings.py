ae66ae301d57adeb6c233fe43ee2dd680e75ea2d
Milton <madandat@gmail.com>
2014-01-29 14:31:39 +0200

    bootstrapify the webapp

--- webapp/app/app/settings.py
+++ webapp/app/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'app',
     'south',
+    'compressor',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,5 +83,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'app/static/'), )
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'app/templates/'), )
