35ecd00705686b6d75856c727a671757bd4b9953
Simon de Haan <simon@praekeltfoundation.org>
2014-01-29 11:22:54 +0200

    file layout for user service module

---
+++ archer/users/tests/test_user_service.py
@@ -0,0 +1,13 @@
+from twisted.trial.unittest import TestCase
+
+
+class UserServiceTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_something(self):
+        self.assertEqual(1, 1)
