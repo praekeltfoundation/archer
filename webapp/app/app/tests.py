148e96e83d239a5c77649a0e89e38d4070c6a1c4
Simon de Haan <simon@praekeltfoundation.org>
2014-02-01 10:12:33 +0200

    smaller tests & now fixed

--- webapp/app/app/tests.py
+++ webapp/app/app/tests.py
@@ -55,9 +55,9 @@ class ApiTestCase(TestCase):
         }))
 
         response = authenticate('joesoap', '1234')
-        self.assertEquals(response, json.dumps({
+        self.assertEquals(response, {
             'user_id': uuid.hex
-        }))
+        })
         mock_post.assert_called_with(
             settings.USER_SERVICE_URL, json.dumps({
                 'username': 'joesoap',
@@ -85,39 +85,14 @@ class ApiTestCase(TestCase):
 
     @patch('requests.put')
     @patch('requests.post')
-    def test_registration(self, mock_put, mock_post):
+    def test_view_registration_success(self, mock_post, mock_put):
         uuid = uuid4().hex
-        mock_put.return_value = RequestsResponse(200, json.dumps({
-            'user_id': uuid,
-        }))
         mock_post.return_value = RequestsResponse(200, json.dumps({
             'user_id': uuid
         }))
-
-        client = Client()
-
-        data = {
-            'username': 'joesoap',
-            'password1': '1234',
-            'password2': '1234',
-            'email': 'test@email.com',
-            'mobile_number': '27123456789',
-        }
-
-        resp = client.post('/join/', data)
-        self.assertEquals(resp.status_code, 302)
-        self.assertTrue(User.objects.filter(username='joesoap').exists())
-
-    @patch('requests.post')
-    @patch('requests.put')
-    def test_registration_view(self, mock_put, mock_post):
-        uuid = uuid4().hex
         mock_put.return_value = RequestsResponse(200, json.dumps({
             'user_id': uuid,
         }))
-        mock_post.return_value = RequestsResponse(200, json.dumps({
-            'user_id': uuid,
-        }))
 
         client = Client()
 
@@ -128,34 +103,82 @@ class ApiTestCase(TestCase):
             'email': 'test@email.com',
             'mobile_number': '27123456789',
         }
+
         resp = client.post('/join/', data)
         self.assertEquals(resp.status_code, 302)
         self.assertTrue(User.objects.filter(username='joesoap').exists())
+        mock_put.assert_called_with(
+            settings.USER_SERVICE_URL, json.dumps({
+                'username': 'joesoap',
+                'password': '1234',
+                'email': 'test@email.com',
+                'msisdn': '27123456789'
+            }))
+        mock_post.assert_called_with(
+            settings.USER_SERVICE_URL, json.dumps({
+                'username': 'joesoap',
+                'password': '1234',
+            }))
 
         resp = client.get('/')
         self.assertContains(resp, 'Logout')
 
-        #test api error result
+    @patch('requests.put')
+    @patch('requests.post')
+    def test_view_registration_fail(self, mock_post, mock_put):
+        uuid = uuid4().hex
+        mock_put.result
         mock_put.return_value = RequestsResponse(400)
+        mock_post.return_value = RequestsResponse(200, json.dumps({
+            'user_id': uuid,
+        }))
+
+        data = {
+            'username': 'joesoap',
+            'password1': '1234',
+            'password2': '1234',
+            'email': 'test@email.com',
+            'mobile_number': '27123456789',
+        }
+        client = Client()
         resp = client.post('/join/', data)
         self.assertEquals(resp.status_code, 200)
         self.assertContains(resp, 'A user with that username already exists.')
 
+    @patch('requests.put')
+    @patch('requests.post')
+    def test_view_registration_fail_after_auth(self, mock_post, mock_put):
         #test auth error after register
-        mock_put.return_value = RequestsResponse(200)
-        mock_post.return_value = RequestsResponse(400)
+        uuid = uuid4().hex
+        mock_put.return_value = RequestsResponse(200, json.dumps({
+            'user_id': uuid,
+        }))
+        mock_post.return_value = RequestsResponse(400, json.dumps({
+            'user_id': uuid,
+        }))
 
+        data = {
+            'username': 'joesoap',
+            'password1': '1234',
+            'password2': '1234',
+            'email': 'test@email.com',
+            'mobile_number': '27123456789',
+        }
+
+        client = Client()
         resp = client.post('/join/', data)
         self.assertEquals(resp.status_code, 200)
         self.assertContains(resp, 'Error. Please try again later')
 
-        #Logout and test Login view
-        client.get('/logout/')
+        # #Logout and test Login view
+        # client.get('/logout/')
 
         resp = client.get('/')
         self.assertNotContains(resp, 'Logout')
 
-        mock_post.return_value = RequestsResponse(200)
+        mock_post.return_value = RequestsResponse(200, json.dumps({
+            'user_id': uuid,
+        }))
 
         data = {
             'username': 'joesoap',
