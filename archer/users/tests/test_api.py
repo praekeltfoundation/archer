6c5262e1d99bdb7577e45d04962cb40ebc608a1e
Simon de Haan <simon@praekeltfoundation.org>
2014-02-08 09:30:08 +0200

    5s timeout for travis

--- archer/users/tests/test_api.py
+++ archer/users/tests/test_api.py
@@ -17,7 +17,7 @@ DelayedCall.debug = True
 
 class TestUserServiceApp(TestCase):
 
-    timeout = 1
+    timeout = 5
 
     def setUp(self):
         self.patch(UserServiceApp, 'make_uuid', lambda _: 'uuid')
